06e2dc99b1cc17ed2286a0cc8dfb95c6925d6525
Alex Siegberg <alex.siegberg@gmail.com>
2019-06-28 15:28:23 -0700

    add function to output galactic age on each planet

--- src/main.js
+++ src/main.js
@@ -4,7 +4,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+function displayGalacticAges(person, age, averageLife, planets) {
+  planets.forEach(function (planet) {
+    console.log('Your age on planet ' + planet + ' is ' + person.galacticAge(age, planet) + '.');
+    console.log('You have approximately ' + person.lifeExpectency(age, averageLife, planet) + ' years left to live on ' + planet + '.');
+  });
+}
+
 $(document).ready(function () {
-  var userBirthdate = new AgeCalculator('August 25, 2049');
-  const age = userBirthdate.checkBirthdate();
+  const planetsList = ['Mercury', 'Venus', 'Mars', 'Jupiter'];
+  let user = new AgeCalculator('July 17, 1990');
+  const age = user.checkBirthdate();
+  const averageLifeSpan = 80;
+
+  displayGalacticAges(user, age, averageLifeSpan, planetsList);
 });
