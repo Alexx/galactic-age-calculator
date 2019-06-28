7cbec8129ef211549a031be9b96cff7ff0d8eb45
Alex Siegberg <alex.siegberg@gmail.com>
2019-06-28 15:41:59 -0700

    add feature to detect if user lived past average life expectency

--- src/main.js
+++ src/main.js
@@ -7,15 +7,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function displayGalacticAges(person, age, averageLife, planets) {
   planets.forEach(function (planet) {
     console.log('Your age on planet ' + planet + ' is ' + person.galacticAge(age, planet) + '.');
-    console.log('You have approximately ' + person.lifeExpectency(age, averageLife, planet) + ' years left to live on ' + planet + '.');
+
+    let yearsLeft = person.lifeExpectency(age, averageLife, planet);
+
+    if (yearsLeft > 0) {
+      console.log('You have approximately ' + yearsLeft + ' years left to live on ' + planet + '.');
+    } else {
+      console.log('You have lived ' + Math.abs(yearsLeft) + ' past your expected life expectency!');
+    }
   });
 }
 
 $(document).ready(function () {
   const planetsList = ['Mercury', 'Venus', 'Mars', 'Jupiter'];
-  let user = new AgeCalculator('July 17, 1990');
-  const age = user.checkBirthdate();
+
+  //Enter your birthdate below
+  const userBirthDate = 'July 17, 1890';
+
+  //Enter your expected life span below
   const averageLifeSpan = 80;
+
+  let user = new AgeCalculator(userBirthDate);
+  const age = user.checkBirthdate();
 
   displayGalacticAges(user, age, averageLifeSpan, planetsList);
 });
